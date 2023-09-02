cc870b90e9df0c64244eaa198b758b87a5d9ef6b
Jakub Sus <s22696@pjwstk.edu.pl>
2023-09-02 02:18:16 +0200

    SeedData

--- Training-and-diet-backend/Training-and-diet-backend/Context/ApplicationDbContext.cs
+++ Training-and-diet-backend/Training-and-diet-backend/Context/ApplicationDbContext.cs
@@ -15,5 +15,109 @@ namespace Training_and_diet_backend.Context
         public DbSet<Trainee_exercise> Trainee_exercises { get; set; }
         public DbSet<Training_plan> Training_plans { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var user = new User { Id_User = 1, Role = UserRole.Trainer, Name = "Michał", Last_name = "Emczyk", Email = "michal@gmail.com", Phone_number = "48777888777", Email_validated = true, Sex = "Male" };
+            var user1 = new User { Id_User = 2, Role = UserRole.Pupil, Name = "Anna", Last_name = "Kowalska", Email = "anna@gmail.com", Phone_number = "48666778888", Email_validated = true, Sex = "Female" };
+            var user2 = new User { Id_User = 3, Role = UserRole.Pupil, Name = "John", Last_name = "Doe", Email = "john@gmail.com", Phone_number = "48555667777", Email_validated = true, Sex = "Male" };
+
+            var exercise = new Exercise
+            {
+                Id_Exercise = 1,
+                Name = "Pompki",
+                Details = "Podstawowe ćwiczenie siłowe, angażuje mięśnie klatki piersiowej, ramion i tricepsa.",
+                Exercise_steps = "[{\"Step\": 1, \"Description\": \"Połóż się na podłodze na brzuchu, ręce ustawione nieco szerzej niż szerokość ramion.\"}, {\"Step\": 2, \"Description\": \"Wypnij się na rękach i naciskaj ciało do góry, wyprostowując ręce.\"}, {\"Step\": 3, \"Description\": \"Powoli wróć do pozycji wyjściowej, zginając ręce w łokciach.\"}]",
+                Image = null,
+                Id_Trainer = 1
+            };
+
+            var exercise1 = new Exercise
+            {
+                Id_Exercise = 2,
+                Name = "Przysiady",
+                Details = "Ćwiczenie wzmacniające mięśnie nóg, pośladków i dolnej części pleców.",
+                Exercise_steps = "[{\"Step\": 1, \"Description\": \"Stań prosto, nogi ustawione na szerokość bioder.\"}, {\"Step\": 2, \"Description\": \"Opuszczaj biodra w dół, jakbyś siadał na niewidzialne krzesło.\"}, {\"Step\": 3, \"Description\": \"Powoli wracaj do pozycji wyjściowej, naciskając pięty w podłogę.\"}]",
+                Image = null,
+                Id_Trainer = 1
+            };
+
+            var exercise2 = new Exercise
+            {
+                Id_Exercise = 3,
+                Name = "Plank",
+                Details = "Ćwiczenie wzmacniające mięśnie brzucha, pleców i ramion.",
+                Exercise_steps = "[{\"Step\": 1, \"Description\": \"Połóż się na brzuchu, opierając się na przedramionach i palcach u stóp.\"}, {\"Step\": 2, \"Description\": \"Utrzymuj prostą linię od głowy do pięt, napinając mięśnie brzucha.\"}, {\"Step\": 3, \"Description\": \"Utrzymuj tę pozycję przez określony czas.\"}]",
+                Image = null,
+                Id_Trainer = 1
+            };
+
+
+            var trainingPlan = new Training_plan
+            {
+                Id_Training_plan = 1,
+                Name = "Plan treningowy dla początkujących",
+                Type = "Siłowy",
+                Start_date = new DateTime(2023, 09, 10),
+                End_date = new DateTime(2023, 09, 30),
+                Plan_duration = TimeSpan.FromDays(21),
+                Id_Trainer = 1,
+                Id_Pupil = 2
+            };
+
+            var trainingPlan1 = new Training_plan
+            {
+                Id_Training_plan = 2,
+                Name = "Plan treningowy na odchudzanie",
+                Type = "Cardio",
+                Start_date = new DateTime(2023, 10, 1),
+                End_date = new DateTime(2023, 10, 31),
+                Plan_duration = TimeSpan.FromDays(31),
+                Id_Trainer = 1,
+                Id_Pupil = 2
+            };
+
+
+
+            var traineeExercise = new Trainee_exercise
+            {
+                Id_Trainee_exercise = 1,
+                Series_number = 3,
+                Repetitions_number = 12,
+                Date = new DateTime(2023, 09, 12),
+                Exercise_duration = TimeSpan.FromMinutes(30),
+                Id_Exercise = 1,
+                Id_Training_plan = 1
+            };
+
+            var traineeExercise1 = new Trainee_exercise
+            {
+                Id_Trainee_exercise = 2,
+                Series_number = 4,
+                Repetitions_number = 10,
+                Date = new DateTime(2023, 09, 15),
+                Exercise_duration = TimeSpan.FromMinutes(35),
+                Id_Exercise = 2,
+                Id_Training_plan = 1
+            };
+
+            var traineeExercise2 = new Trainee_exercise
+            {
+                Id_Trainee_exercise = 3,
+                Series_number = 2,
+                Repetitions_number = 15,
+                Date = new DateTime(2023, 09, 20),
+                Exercise_duration = TimeSpan.FromMinutes(25),
+                Id_Exercise = 3,
+                Id_Training_plan = 2
+            };
+
+
+            modelBuilder.Entity<User>().HasData(user, user1, user2);
+            modelBuilder.Entity<Exercise>().HasData(exercise, exercise1, exercise2);
+            modelBuilder.Entity<Training_plan>().HasData(trainingPlan, trainingPlan1);
+            modelBuilder.Entity<Trainee_exercise>().HasData(traineeExercise, traineeExercise1, traineeExercise2);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ Training-and-diet-backend/Training-and-diet-backend/Data/Migrations/20230902001447_SeedData.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Training_and_diet_backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id_User", "Age", "Email", "Email_validated", "Height", "Last_name", "Name", "Phone_number", "Role", "Sex", "Weight" },
+                values: new object[,]
+                {
+                    { 1, null, "michal@gmail.com", true, null, "Emczyk", "Michał", "48777888777", 3, "Male", null },
+                    { 2, null, "anna@gmail.com", true, null, "Kowalska", "Anna", "48666778888", 2, "Female", null },
+                    { 3, null, "john@gmail.com", true, null, "Doe", "John", "48555667777", 2, "Male", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Exercises",
+                columns: new[] { "Id_Exercise", "Details", "Exercise_steps", "Id_Trainer", "Image", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Podstawowe ćwiczenie siłowe, angażuje mięśnie klatki piersiowej, ramion i tricepsa.", "[{\"Step\": 1, \"Description\": \"Połóż się na podłodze na brzuchu, ręce ustawione nieco szerzej niż szerokość ramion.\"}, {\"Step\": 2, \"Description\": \"Wypnij się na rękach i naciskaj ciało do góry, wyprostowując ręce.\"}, {\"Step\": 3, \"Description\": \"Powoli wróć do pozycji wyjściowej, zginając ręce w łokciach.\"}]", 1, null, "Pompki" },
+                    { 2, "Ćwiczenie wzmacniające mięśnie nóg, pośladków i dolnej części pleców.", "[{\"Step\": 1, \"Description\": \"Stań prosto, nogi ustawione na szerokość bioder.\"}, {\"Step\": 2, \"Description\": \"Opuszczaj biodra w dół, jakbyś siadał na niewidzialne krzesło.\"}, {\"Step\": 3, \"Description\": \"Powoli wracaj do pozycji wyjściowej, naciskając pięty w podłogę.\"}]", 1, null, "Przysiady" },
+                    { 3, "Ćwiczenie wzmacniające mięśnie brzucha, pleców i ramion.", "[{\"Step\": 1, \"Description\": \"Połóż się na brzuchu, opierając się na przedramionach i palcach u stóp.\"}, {\"Step\": 2, \"Description\": \"Utrzymuj prostą linię od głowy do pięt, napinając mięśnie brzucha.\"}, {\"Step\": 3, \"Description\": \"Utrzymuj tę pozycję przez określony czas.\"}]", 1, null, "Plank" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Training_plans",
+                columns: new[] { "Id_Training_plan", "End_date", "Id_Pupil", "Id_Trainer", "Name", "Plan_duration", "Start_date", "Type" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2023, 9, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 1, "Plan treningowy dla początkujących", new TimeSpan(21, 0, 0, 0, 0), new DateTime(2023, 9, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Siłowy" },
+                    { 2, new DateTime(2023, 10, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 1, "Plan treningowy na odchudzanie", new TimeSpan(31, 0, 0, 0, 0), new DateTime(2023, 10, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Cardio" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Trainee_exercises",
+                columns: new[] { "Id_Trainee_exercise", "Comments", "Date", "Exercise_duration", "Id_Exercise", "Id_Training_plan", "Repetitions_number", "Series_number" },
+                values: new object[,]
+                {
+                    { 1, null, new DateTime(2023, 9, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 30, 0, 0), 1, 1, 12, 3 },
+                    { 2, null, new DateTime(2023, 9, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 35, 0, 0), 2, 1, 10, 4 },
+                    { 3, null, new DateTime(2023, 9, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 25, 0, 0), 3, 2, 15, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Trainee_exercises",
+                keyColumn: "Id_Trainee_exercise",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Trainee_exercises",
+                keyColumn: "Id_Trainee_exercise",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Trainee_exercises",
+                keyColumn: "Id_Trainee_exercise",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id_User",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id_Exercise",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id_Exercise",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id_Exercise",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Training_plans",
+                keyColumn: "Id_Training_plan",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Training_plans",
+                keyColumn: "Id_Training_plan",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id_User",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id_User",
+                keyValue: 2);
+        }
+    }
+}
