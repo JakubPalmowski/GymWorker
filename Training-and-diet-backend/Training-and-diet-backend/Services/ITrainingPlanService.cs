bd788d716f4169bb3fd71012f5d93bcea59e5fac
Jakub Sus <s22696@pjwstk.edu.pl>
2023-09-03 01:29:21 +0200

    AddGetExercisesFromTrainingPlanRequest

--- Training-and-diet-backend/Training-and-diet-backend/Services/ITrainingPlanService.cs
+++ Training-and-diet-backend/Training-and-diet-backend/Services/ITrainingPlanService.cs
@@ -1,10 +1,14 @@
-﻿using Training_and_diet_backend.Models;
+﻿using System.Numerics;
+using Training_and_diet_backend.DTOs;
+using Training_and_diet_backend.Models;
 
 namespace Training_and_diet_backend.Services
 {
     public interface ITrainingPlanService
     {
         Task AddTrainingPlan(Training_plan training_Plan);
-     
+        Task<List<GetExerciseGeneralInfoDTO>> GetExercisesFromTrainingPlan(int id_training_plan);
+
+
     }
 }
