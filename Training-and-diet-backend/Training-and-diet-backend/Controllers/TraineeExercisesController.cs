7b933b665583224cb8d09bc0d60f89db6a309325
Filip Wozniak <s22703@pjwstk.edu.pl>
2023-09-03 02:59:13 +0200

    Added Post to TraineeExerciseController

---
+++ Training-and-diet-backend/Training-and-diet-backend/Controllers/TraineeExercisesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Training_and_diet_backend.DTOs;
+using Training_and_diet_backend.Models;
+using Training_and_diet_backend.Services;
+
+namespace Training_and_diet_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TraineeExercisesController : ControllerBase
+    {
+        private readonly ITraineeExercisesService _service;
+        public TraineeExercisesController(ITraineeExercisesService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> AddTraineeExercises([FromBody] PostTraineeExerciseDTO traineeExercise)
+        {
+            if (traineeExercise == null)
+                return BadRequest("Post should have a body");
+
+            var data = new Trainee_exercise
+            {
+                Series_number = traineeExercise.Series_Number,
+                Repetitions_number = traineeExercise.Repetitions_number,
+                Comments = traineeExercise.Comments,
+                Date = traineeExercise.Date, 
+                Id_Exercise = traineeExercise.Id_Exercise,
+                Id_Training_plan = traineeExercise.Id_Training_plan
+
+            };
+
+            await _service.AddTraineeExercises(data);
+            return Ok(data.Id_Exercise);
+        }
+    }
+}
---
+++ Training-and-diet-backend/Training-and-diet-backend/DTOs/PostTraineeExerciseDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Training_and_diet_backend.DTOs
+{
+    public class PostTraineeExerciseDTO
+    {
+        public int Series_Number { get; set; }
+        public int Repetitions_number { get; set; }
+        public string? Comments { get; set; }
+        public DateTime Date { get; set; }
+        public int Id_Exercise { get; set; }
+        public int Id_Training_plan { get; set; }
+
+    }
+}
