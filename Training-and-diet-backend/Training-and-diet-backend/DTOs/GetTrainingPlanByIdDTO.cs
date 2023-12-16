b9febece2846069237b648a6c03ea24f2fdc8bdf
Filip Wozniak <s22703@pjwstk.edu.pl>
2023-12-16 13:17:17 +0100

    Fixed data formatting for training_plan

--- Training-and-diet-backend/Training-and-diet-backend/DTOs/GetTrainingPlanByIdDTO.cs
+++ Training-and-diet-backend/Training-and-diet-backend/DTOs/GetTrainingPlanByIdDTO.cs
@@ -1,12 +1,18 @@
-﻿namespace Training_and_diet_backend.DTOs
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Training_and_diet_backend.DTOs
 {
     public class GetTrainingPlanByIdDTO
     {
         public int Id_Training_plan { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateTime Start_date { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateTime End_date { get; set; }
 
     }
 }
