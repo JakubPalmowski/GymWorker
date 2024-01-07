244fec0b20ee872105346ca282d01f01a3223c27
sus151 <s22696@pjwstk.edu.pl>
2024-01-07 17:52:05 +0100

    AddMyProfileImplementation

--- Training-and-diet-frontend/Training-and-diet-frontend/src/app/components/myProfile/my-profile/my-profile.component.ts
+++ Training-and-diet-frontend/Training-and-diet-frontend/src/app/components/myProfile/my-profile/my-profile.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PupilPersonalInfo } from 'src/app/models/pupilPersonalInfo';
+import { PupilProfile } from 'src/app/models/pupilProfile';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
   styleUrls: ['./my-profile.component.css']
 })
-export class MyProfileComponent {
+export class MyProfileComponent implements OnInit {
+
+
+constructor(private userSerive: UserService) {}
+
+  pupil: PupilPersonalInfo|undefined;
+
+  ngOnInit(): void {
+    //Po dodaniu uwierzytelnienia trzeba będzie pobrać dane zalogowanego użytkownika z jwt Tokena
+    var role = "Pupil"
+    var id = "2";
+
+    if (role == "Pupil") {
+      this.userSerive.GetPupilPersonalInfoById(id).subscribe(
+        {
+          next:(pupilInfo)=>{
+            this.pupil=pupilInfo;
+          },
+          error: (response)=>{
+            console.log(response);
+          }
+        }
+      )
+    }
+    else if (role == "Trainer") {
+      //Pobranie danych mentora
+      //Pobranie danych osobowych mentora
+    }else if (role == "Dietician") {
+      //Pobranie danych mentora
+      //Pobranie danych osobowych mentora
+    }
+    else if (role == "Dietician-Trainer") {
+      //Pobranie danych mentora
+      //Pobranie danych osobowych mentora
+    }
+    
+  }
 
 }
---
+++ Training-and-diet-frontend/Training-and-diet-frontend/src/app/models/pupilPersonalInfo.ts
@@ -0,0 +1,13 @@
+export interface PupilPersonalInfo{
+    name: string;
+    lastName: string;
+    role: string;
+    email: string;
+    emailValidated: boolean;
+    phoneNumber?: string;
+    weight?: number;
+    height?: number;
+    age?: number;
+    sex?: string;
+    bio?: string;
+}
