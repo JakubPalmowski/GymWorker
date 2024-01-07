336c6092e80f7fb0a774961aee3ea337195ebcd1
Filip Wozniak <s22703@pjwstk.edu.pl>
2024-01-07 22:22:46 +0100

    fix

--- Training-and-diet-backend/TrainingAndDietApp.Application/Handlers/User/GetPupilPersonalInfoQueryHandler.cs
+++ Training-and-diet-backend/TrainingAndDietApp.Application/Handlers/User/GetPupilPersonalInfoQueryHandler.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
+using TrainingAndDietApp.Application.Exceptions;
 using TrainingAndDietApp.Application.Queries.User;
 using TrainingAndDietApp.Application.Responses.Pupil;
 using TrainingAndDietApp.Domain.Abstractions;
@@ -11,20 +12,19 @@ namespace TrainingAndDietApp.Application.Handlers.User
 {
     public class GetPupilPersonalInfoQueryHandler : IRequestHandler<GetPupilPersonalInfoQuery, PupilPersonalInfoResponse>
     {
-        private readonly IUserRepository _userRespository;
+        private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        public GetPupilPersonalInfoQueryHandler(IUserRepository userRespository, IMapper mapper)
+        public GetPupilPersonalInfoQueryHandler(IUserRepository userRepository, IMapper mapper)
         {
-            _userRespository = userRespository;
+            _userRepository = userRepository;
             _mapper = mapper;
         }
         public async Task<PupilPersonalInfoResponse> Handle(GetPupilPersonalInfoQuery request, CancellationToken cancellationToken)
         {
-            var pupil = await _userRespository.GetUserByIdAsync(request.Id, cancellationToken);
+            var pupil = await _userRepository.GetUserByIdAsync(request.Id, cancellationToken);
             if (pupil == null)
-            {
-                throw new Exception("Pupil not found");
-            }
+                throw new NotFoundException("Pupil not found");
+            
             var pupilResponse = _mapper.Map<PupilPersonalInfoResponse>(pupil);
             return pupilResponse;
         }
