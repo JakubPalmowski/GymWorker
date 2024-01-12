01497eeedd818304f9752d7a694b3badf4a6ee25
Filip Wozniak <s22703@pjwstk.edu.pl>
2024-01-12 19:15:53 +0100

    staged

---
+++ Training-and-diet-backend/TrainingAndDietApp.Application/Commands/TraineeExercises/DeleteTraineeExerciseCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace TrainingAndDietApp.Application.Commands.TraineeExercises;
+
+public record DeleteTraineeExerciseCommand (int IdTraineeExercise) : IRequest
+{
+    
+}
---
+++ Training-and-diet-backend/TrainingAndDietApp.Application/Handlers/TraineeExercise/DeleteTraineeExerciseCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using TrainingAndDietApp.Application.Commands.TraineeExercises;
+using TrainingAndDietApp.Application.Exceptions;
+using TrainingAndDietApp.Domain.Abstractions;
+
+namespace TrainingAndDietApp.Application.Handlers.TraineeExercise;
+
+public class DeleteTraineeExerciseCommandHandler : IRequestHandler<DeleteTraineeExerciseCommand>
+{
+    private readonly ITraineeExercisesRepository _traineeExercisesRepository;
+    private readonly IMapper _mapper;
+
+    public DeleteTraineeExerciseCommandHandler(ITraineeExercisesRepository traineeExercisesRepository, IMapper mapper)
+    {
+        _traineeExercisesRepository = traineeExercisesRepository;
+        _mapper = mapper;
+    }
+    public async Task Handle(DeleteTraineeExerciseCommand request, CancellationToken cancellationToken)
+    {
+        var traineeExerciseToDelete = await _traineeExercisesRepository.GetTraineeExerciseByIdAsync(request.IdTraineeExercise, cancellationToken);
+        if (traineeExerciseToDelete == null)
+            throw new NotFoundException("TraineeExercise not found");
+        await _traineeExercisesRepository.DeleteTraineeExerciseAsync(traineeExerciseToDelete.IdTraineeExercise, cancellationToken);
+    }
+}
