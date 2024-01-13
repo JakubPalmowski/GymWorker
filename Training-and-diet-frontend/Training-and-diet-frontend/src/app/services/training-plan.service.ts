e1bec0849bd705c44ceadbc699e3e95c20d318db
Baltoreq <baltoreq@gmail.com>
2024-01-13 19:48:40 +0100

    mostly finished training plan - trainer side

--- Training-and-diet-frontend/Training-and-diet-frontend/src/app/services/training-plan.service.ts
+++ Training-and-diet-frontend/Training-and-diet-frontend/src/app/services/training-plan.service.ts
@@ -6,6 +6,7 @@ import { NewLineKind } from 'typescript';
 import { FullTrainingPlan } from '../models/full-training-plan.model';
 import { TrainingPlanExercise } from '../models/trainingPlanExercise.model';
 import { TrainingExerciseFull } from '../models/training-exercise-full';
+import { FullTrainingPlanGet } from '../models/full-training-plan-get';
 
 @Injectable({
   providedIn: 'root'
@@ -18,12 +19,16 @@ export class TrainingPlanService {
    return this.http.get<TrainingPlan[]>('https://localhost:7259/api/TrainingPlan/3/trainingPlans');
   }
 
-  getTrainingPlanById(idPlan:string):Observable<FullTrainingPlan>{
-    return this.http.get<FullTrainingPlan>('https://localhost:7259/api/TrainingPlan/'+idPlan);
+  getTrainingPlanById(idPlan:string):Observable<FullTrainingPlanGet>{
+    return this.http.get<FullTrainingPlanGet>('https://localhost:7259/api/TrainingPlan/'+idPlan);
    }
 
   addTrainingPlan(addTrainingPlanRequest: FullTrainingPlan):Observable<FullTrainingPlan>{
     return this.http.post<FullTrainingPlan>('https://localhost:7259/api/TrainingPlan',addTrainingPlanRequest);
+  }
+
+  editTrainingPlan(addTrainingPlanRequest: FullTrainingPlan,idTrainingPlan:string):Observable<FullTrainingPlan>{
+    return this.http.put<FullTrainingPlan>('https://localhost:7259/api/TrainingPlan/'+idTrainingPlan,addTrainingPlanRequest);
   }
 
   getExercisesByPlanId(planId:string):Observable<TrainingExerciseFull[]>{
