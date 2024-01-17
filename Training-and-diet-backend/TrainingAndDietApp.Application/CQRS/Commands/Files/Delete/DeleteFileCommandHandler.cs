d98986b70ba851c7ec896d17891e648a6ad6557f
Filip Woźniak <s22703@pjwstk.edu.pl>
2024-01-17 21:35:04 +0100

    fixed delete file endpoint

--- Training-and-diet-backend/TrainingAndDietApp.Application/CQRS/Commands/Files/Delete/DeleteFileCommandHandler.cs
+++ Training-and-diet-backend/TrainingAndDietApp.Application/CQRS/Commands/Files/Delete/DeleteFileCommandHandler.cs
@@ -9,23 +9,32 @@ public class DeleteFileCommandHandler : IRequestHandler<DeleteFileCommand, bool>
 {
     private readonly IFileService _fileService;
     private readonly IRepository<Domain.Entities.User> _baseRepository;
+    private readonly IUnitOfWork _unitOfWork;
     private readonly IUserRepository _userRepository;
 
-    public DeleteFileCommandHandler(IFileService fileService, IUserRepository userRepository, IRepository<Domain.Entities.User> baseRepository)
+    public DeleteFileCommandHandler(IFileService fileService, IUserRepository userRepository, IRepository<Domain.Entities.User> baseRepository, IUnitOfWork unitOfWork)
     {
         _fileService = fileService;
         _userRepository = userRepository;
         _baseRepository = baseRepository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task<bool> Handle(DeleteFileCommand request, CancellationToken cancellationToken)
     {
         var user = await _baseRepository.GetByIdAsync(request.LoggedUser, cancellationToken);
+        if (user is null || user.ImageUri is null) 
+            return false;
         var userWithGivenImage = await _userRepository.GetUserByImageUri(request.BlobFileName, cancellationToken);
-
-        if(user != null && userWithGivenImage != null && user.IdUser != userWithGivenImage.IdUser)
+        if (userWithGivenImage == null)
             return false;
 
+        if(user.IdUser != userWithGivenImage.IdUser)
+            return false;
+
+        user.ImageUri = null;
+        await _baseRepository.UpdateAsync(user, cancellationToken);
+        await _unitOfWork.CommitAsync(cancellationToken);
         return await _fileService.DeleteFileAsync(request.BlobFileName);
     }
 }
