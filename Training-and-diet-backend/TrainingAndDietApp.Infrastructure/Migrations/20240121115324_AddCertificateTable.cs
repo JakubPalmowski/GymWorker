67a48383bd12777228be8aac171a31ffd5f3cd89
Jakub Sus <s22696@pjwstk.edu.pl>
2024-01-21 12:57:55 +0100

    AddMigration

---
+++ Training-and-diet-backend/TrainingAndDietApp.Infrastructure/Migrations/20240121115324_AddCertificateTable.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TrainingAndDietApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCertificateTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAccepted",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Certificate",
+                columns: table => new
+                {
+                    IdCertificate = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PdfUri = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    AddedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsAccepted = table.Column<bool>(type: "boolean", nullable: false),
+                    IdMentor = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Certificate", x => x.IdCertificate);
+                    table.ForeignKey(
+                        name: "FK_Certificate_Users_IdMentor",
+                        column: x => x.IdMentor,
+                        principalTable: "Users",
+                        principalColumn: "IdUser",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 1,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 2,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 3,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 4,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 5,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 6,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 7,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 8,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 9,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 10,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 11,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 12,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 13,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 14,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 15,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 16,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 17,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 18,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 19,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 20,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 21,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 22,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 23,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 24,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: 25,
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Certificate_IdMentor",
+                table: "Certificate",
+                column: "IdMentor");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Certificate");
+
+            migrationBuilder.DropColumn(
+                name: "IsAccepted",
+                table: "Users");
+        }
+    }
+}
