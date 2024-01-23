1918fc76955fa4af153d2b484717e890ae4c8b6e
Jakub Sus <s22696@pjwstk.edu.pl>
2024-01-23 23:22:30 +0100

    AddStatusToPupilMentorTable

---
+++ Training-and-diet-backend/TrainingAndDietApp.Infrastructure/Migrations/20240123222002_AddStatusColumnToPupilMentorTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainingAndDietApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusColumnToPupilMentorTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAccepted",
+                table: "Pupil_mentors",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Pupil_mentors",
+                keyColumns: new[] { "IdMentor", "IdPupil" },
+                keyValues: new object[] { 1, 2 },
+                column: "IsAccepted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Pupil_mentors",
+                keyColumns: new[] { "IdMentor", "IdPupil" },
+                keyValues: new object[] { 1, 3 },
+                column: "IsAccepted",
+                value: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAccepted",
+                table: "Pupil_mentors");
+        }
+    }
+}
