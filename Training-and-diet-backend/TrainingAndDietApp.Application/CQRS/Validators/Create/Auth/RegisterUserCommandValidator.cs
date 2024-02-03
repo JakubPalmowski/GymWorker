0ed2baf2b522f772ad06a37cdb2029336087fc06
Filip Woźniak <s22703@pjwstk.edu.pl>
2024-02-03 12:59:32 +0100

    added registerUser Validation

--- Training-and-diet-backend/TrainingAndDietApp.Application/CQRS/Validators/Create/Auth/RegisterUserCommandValidator.cs
+++ Training-and-diet-backend/TrainingAndDietApp.Application/CQRS/Validators/Create/Auth/RegisterUserCommandValidator.cs
@@ -7,7 +7,26 @@ public class RegisterUserCommandValidator : AbstractValidator<RegisterUserComman
 {
     public RegisterUserCommandValidator()
     {
-        
-            
+        RuleFor(u => u.Email )
+            .NotEmpty().WithMessage("Email jest wymagany.")
+            .EmailAddress().WithMessage("Niepoprawny format adresu email.");
+
+        RuleFor(u => u.Password)
+            .NotEmpty().WithMessage("Hasło jest wymagane.")
+            .Matches("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}");
+
+        RuleFor(u => u.Name)
+            .NotEmpty().WithMessage("Imię jest wymagane.")
+            .MinimumLength(2).WithMessage("Imię musi zawierać minimum 2 znaki.")
+            .MaximumLength(50).WithMessage("Imię może zawierać maksymalnie 50 znaków.");
+
+        RuleFor(u => u.LastName)
+            .NotEmpty().WithMessage("Nazwisko jest wymagane.")
+            .MinimumLength(2).WithMessage("Nazwisko musi zawierać minimum 2 znaki.")
+            .MaximumLength(50).WithMessage("Nazwisko może zawierać maksymalnie 50 znaków.");
+
+        RuleFor(u => u.RoleId)
+            .NotEmpty().WithMessage("Rola jest wymagana.");
+
     }
 }
