25feb6605d5ad10b135aa4d3e1dabd7a4dc88dec
Filip Woźniak <s22703@pjwstk.edu.pl>
2024-02-03 12:53:39 +0100

    deleted phoneNumberFrom RegisterUserCommand

---
+++ Training-and-diet-backend/TrainingAndDietApp.Application/CQRS/Validators/Create/Auth/RegisterUserCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using TrainingAndDietApp.Application.CQRS.Commands.Auth.Register;
+
+namespace TrainingAndDietApp.Application.CQRS.Validators.Create.Auth;
+
+public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+{
+    public RegisterUserCommandValidator()
+    {
+        
+            
+    }
+}
